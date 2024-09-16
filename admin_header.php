01a11ec74f5a3421c82cfafd3b264dbf22fbe1ed
Muhammad Imran <mmimran.self@gmail.com>
2024-09-16 16:48:35 +0500

    admin header

---
+++ admin_header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Header</title>
+  <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->
+</head>
+
+<body>
+  <ul class="header">
+    <li><a href="statistics.php">Statistics</a></li>
+    <li><a href="all_tasks.php">All Tasks</a></li>
+    <li><a href="#" id="logout-link">Logout</a></li>
+  </ul>
+
+  <!-- Modal HTML -->
+  <div id="logoutModal" class="modal">
+    <div class="modal-content">
+      <p>Are you sure you want to log out?</p>
+      <button id="confirmLogout">Yes</button>
+      <button id="cancelLogout">No</button>
+    </div>
+  </div>
+
+  <!-- Link the external JS file -->
+  <script src="script.js"></script>
+</body>
+
+</html>
